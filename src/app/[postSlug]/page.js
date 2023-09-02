7e1134d72d8550307256d90e8507bd31e5be78dd
Mike Turner <mike.turner@anu.edu.au>
2023-09-02 18:11:12 +0800

    Renders the DivisionGroupsDemo (exercise 5a)

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import dynamic from 'next/dynamic';
 
 import BlogHero from '@/components/BlogHero';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 import { loadBlogPost } from '@/helpers/file-helpers';
 import CodeSnippet from '@/components/CodeSnippet';
 import styles from './postSlug.module.css';
+
+// Lazy load the DivisionGroupDemo
+const DivisionGroupsDemo = dynamic(() =>
+  import('@/components/DivisionGroupsDemo')
+);
 
 export async function generateMetadata({ params }) {
   const { frontmatter } = await loadBlogPost(params.postSlug);
@@ -24,7 +30,10 @@ async function BlogPost({ params }) {
         publishedOn={frontmatter.publishedOn}
       />
       <div className={styles.page}>
-        <MDXRemote source={content} components={{ pre: CodeSnippet }} />
+        <MDXRemote
+          source={content}
+          components={{ pre: CodeSnippet, DivisionGroupsDemo }}
+        />
       </div>
     </article>
   );
